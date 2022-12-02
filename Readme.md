e01cb9bad98e33f931d414a97c1f464e6c352225
Beatriz Soares <lemosbea@hotmail.com>
2022-12-02 19:17:13 -0300

    ProjetoLampada

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Olá, tudo bem? 
+
+Esse projeto consiste em um simples simulador de uma lâmpada, contendo botão de "ativar" e "desabilitar" a imagem da lâmpada em questão. O botão em pauta quando acionado em "Ligar" se transforma em "Desligar", fazendo trocas de imagens (JS), além de dar duplo click, trocando para a imagem da lâmpada "quebrada". Essas funções foram elaboradas em JavaScript puro. 
+
+O Projeto Lâmpada foi criado e inspirado seguido pela orientação do professor Fernando Leonid, acompanhando alterações pessoais no HTML, CSS e JS.
---
+++ assets/script.js
@@ -0,0 +1,39 @@
+const turnOnOff = document.getElementById ("turnOnOff");
+const lamp = document.getElementById ("lamp");
+
+function isLampBroken () {
+    return lamp.src.indexOf ("quebrada") > -1
+}
+
+function lampOn () {
+    if (!isLampBroken ()) {
+    lamp.src ="assets/img/ligada.jpg";
+    }
+}
+
+function lampOff (){
+    if (!isLampBroken ()) {
+    lamp.src ="assets/img/desligada.jpg";
+}
+}
+
+function lampBroken (){
+    lamp.src ="assets/img/quebrada.jpg";
+}
+
+function lampOnOff (){
+    if (turnOnOff.textContent == "Ligar") {
+        lampOn();
+        turnOnOff.textContent = "Desligar";
+    } else{
+        lampOff();
+        turnOnOff.textContent = "Ligar"; 
+
+    }
+}
+
+turnOnOff.addEventListener ("click", lampOnOff);
+
+/*lamp.addEventListener ("mouseover", lampOn);
+lamp.addEventListener ("mouseleave", lampOff);*/
+lamp.addEventListener ("dblclick", lampBroken);
